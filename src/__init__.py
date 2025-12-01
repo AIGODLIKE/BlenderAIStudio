3b55984c607fe78db3d027c55050cc8f5a36013d
karrcharon <karrycharon@qq.com>
2025-12-01 17:41:05 +0800

    add i18n

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from .logger import logger
 
 modules = [
     # 模块列表
+    "i18n",
 ]
 
 reg, unreg = bpy.utils.register_submodule_factory(__package__, modules)
---
+++ src/i18n/translations/zh_HANS.py
@@ -0,0 +1,7 @@
+PROP_TCTX = "BlenderAIStudioPropTCTX"
+PANEL_TCTX = "BlenderAIStudioPanelTCTX"
+
+translations = (
+    # ("Test Property Translation", "测试属性翻译", PROP_TCTX),
+    # ("Test Panel Translation", "测试面板翻译", PANEL_TCTX),
+)
