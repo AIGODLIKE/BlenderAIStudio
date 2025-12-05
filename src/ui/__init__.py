d4e830591ad621ff9801332393e66c43b67067f2
karrcharon <karrycharon@qq.com>
2025-12-05 20:44:34 +0800

    add right top button to open AI Panel;

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -7,16 +7,26 @@ clss = [
     AIStudioPanel,
     AIStudioImagePanel,
     AIStudioHistoryPanel,
-
     SelectMaskMenu,
 ]
 
 register_classes, unregister_classes = bpy.utils.register_classes_factory(clss)
 
 
+def draw_ai_studio_button(self, context):
+    from ..studio.ops import AIStudioEntry
+
+    layout = self.layout
+    col = layout.column()
+    col.alert = True
+    col.operator(AIStudioEntry.bl_idname, text="", icon="LIGHT")
+
+
 def register():
     register_classes()
+    bpy.types.VIEW3D_HT_header.append(draw_ai_studio_button)
 
 
 def unregister():
+    bpy.types.VIEW3D_HT_header.remove(draw_ai_studio_button)
     unregister_classes()
