5e07832047fcf9613367cfb75397c4200efdf2d4
karrcharon <karrycharon@qq.com>
2026-01-29 16:46:33 +0800

    add: preference get/set api_key by model name

--- src/preferences/api_key.py
+++ src/preferences/api_key.py
@@ -4,6 +4,14 @@ from ..utils.property import set_bl_property, get_bl_property
 
 
 class ApiKey:
+    def get_api_key_by_model_name(self, model_name: str):
+        key = f"api_key_{model_name}"
+        return get_bl_property(self, key, "")
+
+    def set_api_key_by_model_name(self, model_name: str, value: str):
+        key = f"api_key_{model_name}"
+        set_bl_property(self, key, value)
+
     def get_api_key(self):
         """获取api key"""
         key = f"api_key_{bpy.context.scene.blender_ai_studio_property.model_name}"
