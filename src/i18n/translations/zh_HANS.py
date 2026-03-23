66f4dc99a379fbc7b76930155b82bf14e1e0993c
karrcharon <karrycharon@qq.com>
2026-03-23 17:13:29 +0800

    update: translation

--- src/i18n/translations/zh_HANS.py
+++ src/i18n/translations/zh_HANS.py
@@ -63,6 +63,7 @@ error_translations = (
      None),
     ("RemoveBackground is submitted, please wait for the result", "移除背景任务已提交，请等待结果", None),
     ("OCR is submitted, please wait for the result", "OCR任务已提交，请等待结果", None),
+    ("Task is submitted, please wait for the result", "任务已提交，请等待结果", None),
 )
 
 studio_panel_translations = (
