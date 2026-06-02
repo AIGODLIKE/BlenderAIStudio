54d47f07a3a52d53c54104bf7e452a6f4c5546c0
小萌新 working <3209970865@qq.com>
2026-06-02 09:17:07 +0800

    update version 0.1.16 -> 0.1.17

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Blender AI Studio",
     "author": "幻之境开发小组",
-    "version": (0, 1, 16),
+    "version": (0, 1, 17),
     "blender": (4, 0, 0),
     "location": "3DView->Panel",
     "category": "AI",
